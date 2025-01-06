34882910ac306a2057e3e446d0a0b0503dcebb3a
varun <varunboddapati553@gmail.com>
2025-01-06 21:51:34 +0530

    html

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{       int a=20;
+int b=10;
+cout<<"The value is "<<a+b<<endl;
+        cout << "hello world"<< endl;
+        cout << "hii"<< endl;
+        
+        return 0;
+}
--- test.py
+++
@@ -1,4 +0,0 @@
-import math as m
-print(2+4)
-print("hello world")
-print(int(m.sqrt(25)))
